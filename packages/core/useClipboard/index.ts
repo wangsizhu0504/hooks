e56136b22c7e6492b79fcc58b7fc1e3589cb4d2d
Kriszu <wangsizhu0504@outlook.com>
2023-05-10 23:25:38 +0800

    feat: add useClipboard and useEventListener

---
+++ packages/core/useClipboard/index.ts
@@ -0,0 +1,124 @@
+/* this implementation is original ported from https://github.com/logaretm/vue-use-web by Abdelrahman Awad */
+
+import { useTimeout } from '@kriszu/hooks'
+import type { ComputedRef, Ref } from 'vue-demi'
+import { computed, ref } from 'vue-demi'
+import { isClient, toValue } from '../utils'
+import type { MaybeRefOrGetter } from '../types'
+import type { WindowEventName } from '../useEventListener'
+import { useEventListener } from '../useEventListener'
+
+const defaultNavigator = isClient ? window.navigator : undefined
+export interface UseClipboardOptions<Source> {
+  /*
+   * Specify a custom `navigator` instance, e.g. working with iframes or in testing environments.
+   */
+  navigator?: Navigator
+  /**
+   * Enabled reading for clipboard
+   *
+   * @default false
+   */
+  read?: boolean
+
+  /**
+   * Copy source
+   */
+  source?: Source
+
+  /**
+   * Milliseconds to reset state of `copied` ref
+   *
+   * @default 1500
+   */
+  copiedDuring?: number
+
+  /**
+   * Whether fallback to document.execCommand('copy') if clipboard is undefined.
+   *
+   * @default false
+   */
+  legacy?: boolean
+}
+
+export interface UseClipboardReturn<Optional> {
+  isSupported: Ref<boolean>
+  text: ComputedRef<string>
+  copied: ComputedRef<boolean>
+  copy: Optional extends true ? (text?: string) => Promise<void> : (text: string) => Promise<void>
+}
+
+/**
+ * Reactive Clipboard API.
+ *
+ * @see https://vueuse.org/useClipboard
+ * @param options
+ */
+export function useClipboard(options?: UseClipboardOptions<undefined>): UseClipboardReturn<false>
+export function useClipboard(options: UseClipboardOptions<MaybeRefOrGetter<string>>): UseClipboardReturn<true>
+export function useClipboard(options: UseClipboardOptions<MaybeRefOrGetter<string> | undefined> = {}): UseClipboardReturn<boolean> {
+  const {
+    navigator = defaultNavigator,
+    read = false,
+    source,
+    copiedDuring = 1500,
+    legacy = false,
+  } = options
+
+  const events = ['copy', 'cut']
+  const isClipboardApiSupported = computed(() => Boolean(navigator && 'clipboard' in navigator))
+  const isSupported = computed(() => isClipboardApiSupported.value || legacy)
+  const text = ref('')
+  const copied = ref(false)
+  const { start } = useTimeout(() => copied.value = false, copiedDuring)
+
+  function updateText() {
+    if (isClipboardApiSupported.value) {
+      navigator!.clipboard.readText().then((value) => {
+        text.value = value
+      })
+    } else {
+      text.value = legacyRead()
+    }
+  }
+
+  if (isSupported.value && read) {
+    for (const event of events)
+      useEventListener(event as WindowEventName, updateText)
+  }
+
+  async function copy(value = toValue(source)) {
+    if (isSupported.value && value != null) {
+      if (isClipboardApiSupported.value)
+        await navigator!.clipboard.writeText(value)
+      else
+        legacyCopy(value)
+
+      text.value = value
+      copied.value = true
+      start()
+    }
+  }
+
+  function legacyCopy(value: string) {
+    const ta = document.createElement('textarea')
+    ta.value = value ?? ''
+    ta.style.position = 'absolute'
+    ta.style.opacity = '0'
+    document.body.appendChild(ta)
+    ta.select()
+    document.execCommand('copy')
+    ta.remove()
+  }
+
+  function legacyRead() {
+    return document?.getSelection?.()?.toString() ?? ''
+  }
+
+  return {
+    isSupported,
+    text: text as ComputedRef<string>,
+    copied: copied as ComputedRef<boolean>,
+    copy,
+  }
+}
---
+++ packages/core/useEventListener/index.md
@@ -0,0 +1,58 @@
+---
+category: Browser
+---
+
+# useEventListener
+
+Use EventListener with ease. Register using [addEventListener](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener) on mounted, and [removeEventListener](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener) automatically on unmounted.
+
+## Usage
+
+```js
+import { useEventListener } from '@kriszu/hooks'
+
+useEventListener(document, 'visibilitychange', (evt) => {
+  console.log(evt)
+})
+```
+
+You can also pass a ref as the event target, `useEventListener` will unregister the previous event and register the new one when you change the target.
+
+```ts
+import { useEventListener } from '@kriszu/hooks'
+
+const element = ref<HTMLDivElement>()
+useEventListener(element, 'keydown', (e) => {
+  console.log(e.key)
+})
+```
+
+```html
+<template>
+  <div v-if="cond" ref="element">Div1</div>
+  <div v-else ref="element">Div2</div>
+</template>
+```
+
+You can also call the returned to unregister the listener.
+
+```ts
+import { useEventListener } from '@kriszu/hooks'
+
+const cleanup = useEventListener(document, 'keydown', (e) => {
+  console.log(e.key)
+})
+
+cleanup() // This will unregister the listener.
+```
+
+Note if you components also run in SSR (Server Side Rendering), you might get errors (like `document is not defined`) because DOM APIs like `document` and `window` are not available in Node.js. To avoid that you can put the logic inside `onMounted` hook.
+
+```ts
+// onMounted will only be called in the client side, so it guarantees the DOM APIs are available.
+onMounted(() => {
+  useEventListener(document, 'keydown', (e) => {
+    console.log(e.key)
+  })
+})
+```
