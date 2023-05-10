abf6f976650bb075d57c9809d0b5700afb517f14
kriszu <wangsizhu0504@outlook.com>
2023-05-10 17:16:26 +0800

    chore: init commit

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@kriszu/hooks': resolve(__dirname, 'packages/core/index.ts')
+    },
+    dedupe: [
+      'vue',
+      'vue-demi',
+      '@vue/runtime-core',
+    ],
+  },
+  define: {
+    __VUE_OPTIONS_API__: 'true',
+    __VUE_PROD_DEVTOOLS__: 'false',
+  },
+  test: {
+    environment: 'jsdom',
+    reporters: 'dot',
+  },
+  ssr: {
+    noExternal: [
+      /@kriszu\/.*/,
+    ],
+  },
+})
