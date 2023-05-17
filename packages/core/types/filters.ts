b2a429efbceb96283b0b64029de820bb70860c98
Kriszu <wangsizhu0504@outlook.com>
2023-05-17 21:36:42 +0800

    fix: type bug

--- packages/core/types/filters.ts
+++ packages/core/types/filters.ts
@@ -1,59 +1,33 @@
-import { shallowRef, unref } from 'vue-demi'
+import type { MaybeRefOrGetter } from 'vue-demi'
+import type { EventFilter } from '.'
 
-interface UseScrollToOptions {
-  el: any
-  to: number
-  duration?: number
-  callback?: () => any
+export type FunctionArgs<Args extends any[] = any[], Return = void> = (...args: Args) => Return
+
+export interface FunctionWrapperOptions<Args extends any[] = any[], This = any> {
+  fn: FunctionArgs<Args, This>
+  args: Args
+  thisArg: This
 }
 
-function easeInOutQuad(t: number, b: number, c: number, d: number) {
-  t /= d / 2
-  if (t < 1)
-    return (c / 2) * t * t + b
-
-  t--
-  return (-c / 2) * (t * (t - 2) - 1) + b
+export interface ConfigurableEventFilter {
+  /**
+   * Filter for if events should to be received.
+   *
+   */
+  eventFilter?: EventFilter
 }
 
-function move(el: HTMLElement, amount: number) {
-  el.scrollTop = amount
+export interface DebounceFilterOptions {
+  /**
+   * The maximum time allowed to be delayed before it's invoked.
+   * In milliseconds.
+   */
+  maxWait?: MaybeRefOrGetter<number>
+
+  /**
+   * Whether to reject the last call if it's been cancel.
+   *
+   * @default false
+   */
+  rejectOnCancel?: boolean
 }
-
-const position = (el: HTMLElement) => {
-  return el.scrollTop
-}
-function useScrollTo({ el, to, duration = 500, callback }: UseScrollToOptions) {
-  const isActiveRef = shallowRef(false)
-  const start = position(el)
-  const change = to - start
-  const increment = 20
-  let currentTime = 0
-
-  const animateScroll = function () {
-    if (!unref(isActiveRef))
-      return
-
-    currentTime += increment
-    const val = easeInOutQuad(currentTime, start, change, duration)
-    move(el, val)
-    if (currentTime < duration && unref(isActiveRef)) {
-      requestAnimationFrame(animateScroll)
-    } else {
-      if (callback && typeof callback === 'function')
-        callback()
-    }
-  }
-  const run = () => {
-    isActiveRef.value = true
-    animateScroll()
-  }
-
-  const stop = () => {
-    isActiveRef.value = false
-  }
-
-  return { start: run, stop }
-}
-
-export { useScrollTo, type UseScrollToOptions }
