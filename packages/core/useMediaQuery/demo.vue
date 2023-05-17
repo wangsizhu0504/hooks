e80f14920c28e863b471ff2c77e703dfaf61c74b
Kriszu <wangsizhu0504@outlook.com>
2023-05-17 11:40:46 +0800

    chore: update

--- packages/core/useMediaQuery/demo.vue
+++ packages/core/useMediaQuery/demo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useMediaQuery } from '@vueuse/core'
+import { useMediaQuery } from '@kriszu/hooks'
 
 const isLargeScreen = useMediaQuery('(min-width: 1024px)')
 const prefersDark = useMediaQuery('(prefers-color-scheme: dark)')
